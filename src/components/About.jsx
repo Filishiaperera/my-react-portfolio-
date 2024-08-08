a39ce6984ff090e8c7c99d0597f3c50183fd73a6
Filishiaperera <nishani.20220043@iit.ac.lk>
2024-08-08 22:25:54 +0530

    added animation and the image to the about me image in the About.jsx file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from '../assets/about.jpg'
+import aboutImg from '../assets/AboutMe.jpeg'
 import{ABOUT_TEXT} from '../constants'
 import {motion} from 'framer-motion'
 const About = () => {
@@ -13,8 +13,8 @@ const About = () => {
         initial={{opacity:0, x:-100}}
         transition={{duration:0.5}}
          className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex items-center justify-center ">
-            <img className='rounded-2xl' src={aboutImg} alt="about me" />
+          <div className="flex items-center justify-center  ">
+            <img className='w-90 h-100  rounded-2xl ' src={aboutImg} alt="about me "  />
           </div>
           </motion.div>
           <motion.div 
@@ -23,7 +23,7 @@ const About = () => {
            transition={{duration:0.5}}
           className='w-full lg:w-1/2'>
             <div className='flex justify-center lg:justify-start'>
-              <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+              <p className='mx-10 my-40 max-w-xl py-20'>{ABOUT_TEXT}</p>
             </div>
           </motion.div>
       </div>
